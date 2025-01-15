2d0ccdfeaaa41c0a573d4ec381346bff08d1d4c7
aaa <49196143+alphs@users.noreply.github.com>
2025-01-15 23:02:50 +0100

    lua/custom: add nvim-treesitter

--- init.lua
+++ init.lua
@@ -9,17 +9,19 @@ vim.opt.nu = true
 vim.opt.shiftwidth = 4
 
 
+-- keymaps
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 vim.keymap.set("n", "-", "<cmd>Ex<CR>")
 
--- Hightlight on yank
+-- autocmds
+
 -- `help: vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("aaa-highligt-yank", { clear = true }),
+	group = vim.api.nvim_create_augroup("highligt-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
